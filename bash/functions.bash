63201174aa3d1b2a94d4e68082ab1f4ca5002d93
FeraxHp <ferax.hp@gmail.com>
2024-04-29 10:57:31 -0500

    funtions.bash:
    - Se añade la funcion activate

--- bash/functions.bash
+++ bash/functions.bash
@@ -7,3 +7,16 @@ bd(){
     cd ..
 }
 
+
+activate() {
+    venv=".venv"
+    if [ "$1" != "" ]; then
+        venv="$1"
+    fi
+    if [ -d "./$venv" ]; then
+        . "./$venv/bin/activate"
+    else
+        echo "There is not a venv directory"
+        echo "Directory name: $venv"
+    fi
+}
