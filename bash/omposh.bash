05aa9bf4623f6871b90bfbbb380056f7aca371b4
Feraxhp <ferax.hp@gmail.com>
2024-05-10 10:46:48 -0500

    General: -Se añade config.fedora, para preinstalar todas las configuraciones de bash -Se elimina desktop.ini  -Se modifica omposh.bash para crear la funcion omp que ejecuta oh-my-posh en la terminal -se agrega unset rc en bash.shell

--- bash/omposh.bash
+++ bash/omposh.bash
@@ -1,5 +1,10 @@
-if command -v oh-my-posh &> /dev/null; then
+
+omp() {
     eval "$(oh-my-posh init bash --config '~/.fleet/themes/ferax.omp.json')"
+}
+
+if command -v oh-my-posh &> /dev/null; then
+    omp
 else
     echo "oh-my-posh is not instaled or is not in your PATH"
 fi
