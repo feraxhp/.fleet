628b1102a07338522a81ba7340734ecd1a1733d5
FeraxHp <ferax.hp@gmail.com>
2024-05-21 13:00:45 -0500

    bash:
    - Se añade fm para abrir el file manager independientemente del desktop que se use (actualemtne solo sopoerta gnome)
    - Se añade el script de instalacion de minecraft
    - Se mejora el stop docker
    
    omp:
    
    - Se agrega un visalizador de docker daemon running
    
    settings:
    
    - Se cambia la letra del editor

--- bash/functions.bash
+++ bash/functions.bash
@@ -7,7 +7,6 @@ bd(){
     cd ..
 }
 
-
 activate() {
     venv=".venv"
     if [ "$1" != "" ]; then
@@ -21,29 +20,16 @@ activate() {
     fi
 }
 
-install-minecraft(){
-    if command -v minecraft &> /dev/null; then
-        echo "Ya esta instalado"
+fm(){
+
+    path="$1"
+    if [ path == "" ]; then
+        path=$(pwd)
+    fi
+
+    if command -v gnome-shell &> /dev/null; then
+        nautilus "$path"
     else
-        mpath="$HOME/.local/bin"
-
-        echo "Creating folder"
-        mkdir -p $mpath
-
-        echo "Downloading..."
-        curl 'https://launcher.mojang.com/download/Minecraft.tar.gz' --output $mpath/minecraft.tar.gz
-
-        echo "Uncompresing..."
-        last=$(pwd)
-        cd $mpath
-        tar -xvzf $mpath/minecraft.tar.gz
-        mv $mpath/minecraft-launcher/minecraft-launcher $mpath/minecraft
-
-        echo "Cleaning..."
-        rm -rf $mpath/minecraft-launcher
-        rm -f minecraft.tar.gz
-        cd $last
-
-        echo "Done!"
+        echo "No file manager found"
     fi
 }
